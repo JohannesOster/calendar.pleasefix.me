63baf77d66144c552115e2959a46930293d8e445
Johannes Oster <johannes.oster@outlook.at>
2024-03-25 08:50:12 +0100

    Add tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<h1>Coming soon ...</h1>
+<h1 class="text-3xl font-bold underline">Coming soon ...</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
