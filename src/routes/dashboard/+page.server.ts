e2e1748cfe6b8e66580e61a8756b9c7415ad1839
Johannes Oster <johannes.oster@outlook.at>
2024-03-28 10:38:33 +0100

    Redirect authenticated users
    If user is already logged in redirect them to dashboard when they press the log in btn.

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -2,7 +2,7 @@ import { getGoogleAuthorizationURL } from '$lib/infrastructure/auth/google.js';
 import { SESSION_COOKIE_KEY } from '$lib/infrastructure/auth/session/session.js';
 import type { Account } from '@prisma/client';
 
-export const load = async ({ locals, cookies }) => {
+export const load = async ({ cookies }) => {
 	const redirectURL = '/dashboard';
 	// Use cookie instead of raw session payload since we neither want to encrypt the payload
 	// here again nor send plain json object to third party.
@@ -13,7 +13,6 @@ export const load = async ({ locals, cookies }) => {
 	const googleAuthorizationURL = getGoogleAuthorizationURL(state);
 
 	return {
-		session: locals.session!,
 		googleAuthorizationURL,
 		connectedAccounts: [] as (Account & { email: string })[]
 	};
